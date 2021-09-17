9bb4b571649fdc07a86ad42245139673932ec3e6
Marvin Knoll <knollmarvin6@gmail.com>
2021-09-17 15:58:43 +0200

    Add storybook with examples

--- doc-viewer/src/App.tsx
+++ doc-viewer/src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
+import FileInput from "./FileInput";
 
 function App() {
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+    const file = files?.[0] || null;
+    if (file) {
+      setFile(file);
+      console.log("file input change", files);
+    }
+  };
+
   return (
     <div className="App">
+      <h1>Hello World</h1>
+      <FileInput handleInputChange={handleInputChange} />
     </div>
   );
 }
